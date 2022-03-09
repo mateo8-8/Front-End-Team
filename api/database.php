1eb47b4b0b3ba64d4b53ace77ebefd2e3b6843ac
Cole Nutter <colenutter@gmail.com>
2022-03-09 15:23:14 -0500

    Create a configuration to connect to the database

--- api/database.php
+++ api/database.php
@@ -1,5 +1,17 @@
 <?php
+$servername = "localhost:3306";
+$username = "wildfire";
+$password = "WildPass";
+$dbname = "wildfire";
 
+//create connection
+$con=mysqli_connect($servername, $username, $password, $dbname);
 
+//Check connection
+
+if (!$con)
+{
+    die("Failed to connect to MySQL: " . mysqli_connect_error());
+}
 
 ?>
