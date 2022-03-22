0609e5ad4eb94f74dd6c8736e0384fa60d47cfc5
Jenny <lion_mango29@outlook.com>
2022-03-22 11:32:37 -0400

    Beautify

--- MySQL/users.sql
+++ MySQL/users.sql
@@ -1,1 +1,8 @@
-CREATE TABLE USERS (userID int, FName varchar(255), LName varchar(255), user varchar(15), password varchar(100), PRIMARY KEY (userID));
+CREATE TABLE USERS (
+    userID int,
+    FName varchar(255),
+    LName varchar(255),
+    user varchar(15),
+    password varchar(100),
+    PRIMARY KEY (userID)
+);
