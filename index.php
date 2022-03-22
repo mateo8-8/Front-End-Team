d16da8ee5efd611f0b342a13919024739f28bde9
mateo8-8 <68440461+mateo8-8@users.noreply.github.com>
2022-03-22 11:43:01 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Front End Demo #1</title>
+</head>
+
+<body>
+	<center>
+		<h1>Login</h1>
+
+		<form action="insert.php" method="get">
+						
+<p>
+				<label for="uName">User Name: </label>
+				<input type="text" name="u_name" id="userName">
+			</p>
+			
+<p>
+				<label for="pword">Password: </label>
+				<input type="text" name="pword" id="pword">
+			</p>			
+			<input type="submit" value="Submit">
+		</form>
+	</center>
+</body>
+
+</html>
