e73a0f77158e92d6980a746ea88aea4d9e9284a6
nuttercd <colenutter@gmail.com>
2022-03-23 19:38:53 -0400

    set password test

---
+++ api/setpassword.php
@@ -0,0 +1,23 @@
+<?php
+
+require('database.php');
+
+$username = strtolower(readline("Enter username: "));
+$password1 = readline("Enter a password: ");
+$password2 - readline("Verify your password: ");
+
+if($password1 == $password2){
+	$hash = password_hash($password1, PASSWORD_DEFAULT);
+	$stms - $con->prepare("UPDATE user SET password = ? WHERE user = ?");
+	$stms->bind_param("ss", $hash, $username);
+	$stms->execute();
+	if($stms->affected_rows == 1){
+		echo "Password Successfully Changed";
+	} else {
+		echo "Password Not Changed";
+	}
+} else {
+	echo "Passwords Do Not Match";
+}
+mysqli_close($con);
+?>
