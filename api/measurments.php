f90faab30985b7f5439b50007325122777827bdf
nuttercd <colenutter@gmail.com>
2022-03-30 10:38:26 -0400

    fix tings

--- api/measurments.php
+++ api/measurments.php
@@ -4,14 +4,14 @@ require('database.php');
 
 $input = json_decode(file_get_contents('php://input'), true);
 
-$measurmentid = $input['data']['measurmentid'];
+$measurementid = $input['data']['measurementid'];
 $nodeid = $input['data']['nodeid'];
 $temp = $input['data']["temperature"];
 $humidity = $input['data']["humidity"];
 $CO2 = $input['data']["CO2"];
 
-$stmt = $con->prepare("INSERT INTO MEASURMENT (MeasurmentID, NodeID, Temperature, Humidity, CO2) VALUES (?, ?, ?, ?, ?)");
-$stmt->bind_param("iiidd", $measurmentid, $nodeid, $temp, $humidity, $CO2);
+$stmt = $con->prepare("INSERT INTO MEASUREMENT (MeasurementID, NodeID, Temperature, Humidity, CO2) VALUES (?, ?, ?, ?, ?)");
+$stmt->bind_param("iiidd", $measurementid, $nodeid, $temp, $humidity, $CO2);
 $stmt->execute();
             
 if ($stmt->affected_rows != 1) {
