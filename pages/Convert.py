d8d8fce831099adde98bbc6b332d3db1ba90e801
Jenny <lion_mango29@outlook.com>
2022-04-07 09:20:28 -0400

    JSON

--- pages/Convert.py
+++ pages/Convert.py
@@ -1,19 +1,6 @@
 import pandas as pd
-import json
-import csv
 
 pdObj = pd.read_json('Data.json', orient='index')
 print(pdObj)
 
-data_file = open('Nodes.csv', 'w', newline='')
-csv_writer = csv.writer(data_file)
-
-count = 0
-for data in pdObj:
-    if count == 0:
-        header = data.keys()
-        csv_writer.writerow(header)
-        count += 1
-    csv_writer.writerow(data.values())
-
-csv_writer.close()
+csvData = pdObj.to_csv('Nodes.csv', index=False)
