01c8c696fad12649f4cf7f462d2ee681b7704c8d
Chad Atkins <91638754+AtkinsChad@users.noreply.github.com>
2022-04-12 12:52:10 -0400

    implement the php mail function

--- api/home.php
+++ api/home.php
@@ -2,6 +2,13 @@
 include "database.php";
 session_start();
 
+$to_email_address = 'atkinschadt@gmail.com';
+$subject = 'First Message';
+$message = 'Hello there';
+
+mail($to_email_address,$subject,$message);
+
+
 // Check user login or not
 if(!isset($_SESSION['uname'])){
     header('Location: index.php');
