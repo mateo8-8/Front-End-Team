91820b7aa518d6b4f2fd20fd28c34488f4718a5d
nuttercd <colenutter@gmail.com>
2022-04-13 15:30:02 -0400

    fix connection typo

--- api/measurementdata.php
+++ api/measurementdata.php
@@ -2,7 +2,7 @@
 
 require('database.php');
 $sql = "SELECT * FROM MEASUREMENT ORDER BY MeasurementID";
-$result = mysqli_query($conn, $sql);
+$result = mysqli_query($con, $sql);
 
 if (mysqli_num_rows($result) > 0) {
   // output data of each row
@@ -17,5 +17,5 @@ if (mysqli_num_rows($result) > 0) {
   echo "0 results";
 }
 
-mysqli_close($conn);
+mysqli_close($con);
 ?>
