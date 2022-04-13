ea8acf48b328079ee3964ed145ddf06e94991952
nuttercd <colenutter@gmail.com>
2022-04-13 15:21:39 -0400

    Test measurement data

--- api/measurmentdata.php
+++ api/measurmentdata.php
@@ -6,13 +6,15 @@ $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
   // output data of each row
+
+  $measurementData = [];
   while($row = mysqli_fetch_assoc($result)) {
-   $measurementData[] = $row;
-   json_encode($measurementData);
+
+   array_push($measurementData,$row);
   }
 } else {
   echo "0 results";
 }
-
+echo json_encode($measurementData);
 mysqli_close($conn);
 ?>
