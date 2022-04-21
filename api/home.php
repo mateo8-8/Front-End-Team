aeae87809ab6e7a07a3be0ebc19edc5cd83fded7
root <root@wildfireproject.us-east4-c.c.sanguine-orb-343017.internal>
2022-04-21 03:50:12 +0000

    home.php

--- api/home.php
+++ api/home.php
@@ -52,6 +52,18 @@ function showNotification() {
 <html>
     <head></head>
     <body onload="initNotifications()">
+	<ul id="nav">
+        	<li><a class="active" href="Home.html">Home</a></li>
+        	<li><a href="About.html">About</a></li>
+        	<li><a href="Search.html">Search</a></li>
+        	<li><a href="Map.html">Map</a></li>
+        	<li><a href="Chart.html">Chart</a></li>
+        	<li><a href="Contact.html">Contact</a></li>
+        	<li><a href="Login.html">Login</a></li>
+    </ul>
+
+    <h2>Home</h2>
+
         <h1>Homepage</h1>
         <form method='post' action="">
             <input type="submit" value="Logout" name="but_logout">
