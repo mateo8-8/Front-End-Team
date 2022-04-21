4c8a72485a35542cd922170aa37abc49946b0ad1
Jenny <lion_mango29@outlook.com>
2022-04-21 08:26:03 -0400

    Update

---
+++ api/email.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Front End Demo #1</title>
+</head>
+
+<body>
+    <center>
+        <h1>Login</h1>
+
+        <form action="subscribe.php" method="GET">
+            <input type="text" placeholder="Email address" name="mail">
+            <button id="subscribe" onclick="pop()">Subscribe</button>
+            <!--             <p>
+                <label for="uName">User Name: </label>
+                <input type="text" name="u_name" id="userName">
+            </p>
+
+            <p>
+                <label for="pword">Password: </label>
+                <input type="text" name="pword" id="pword">
+            </p>
+            <input type="submit" value="Submit"> -->
+        </form>
+    </center>
+</body>
+
+</html>
---
+++ api/subscribe.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Insert Data Page</title>
+</head>
+
+<body>
+    <center>
+        <?php
+
+        // servername => localhost
+        // username => checkout
+        // password => checkout
+        // database name => checkout
+        // before it was aq
+        $conn = mysqli_connect("localhost", "admin", "admin", "email");
+
+        // Check connection
+        if ($conn === false) {
+            die("ERROR: Could not connect. "
+                . mysqli_connect_error());
+        }
+
+        // Taking all 5 values from the form data(input)
+        $usr_name = $_REQUEST['u_name'];
+        $pword = $_REQUEST['pword'];
+
+        // Performing insert query execution
+        // here our table name is college
+        // before I inserted into "checkout"
+        $sql = "INSERT INTO chkout(uname, pword) VALUES ('$usr_name',
+			'$pword')";
+
+        if (mysqli_query($conn, $sql)) {
+            echo "<h3>data stored in a database successfully."
+                . " Please browse your localhost php my admin"
+                . " to view the updated data</h3>";
+
+            //echo nl2br("\n$first_name\n $last_name\n "
+            //	. "$gender\n $address\n $email");
+        } else {
+            echo "ERROR: Hush! Sorry $sql. "
+                . mysqli_error($conn);
+        }
+
+        // Close connection
+        mysqli_close($conn);
+        ?>
+    </center>
+</body>
+
+</html>
