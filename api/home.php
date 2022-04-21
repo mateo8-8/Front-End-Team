6fe6364d97edf91542722c37f3adc603dd1282c5
Jenny <lion_mango29@outlook.com>
2022-04-20 23:58:15 -0400

    home.php

--- api/home.php
+++ api/home.php
@@ -4,12 +4,12 @@ session_start();
 
 
 // Check user login or not
-if(!isset($_SESSION['uname'])){
+if (!isset($_SESSION['uname'])) {
     header('Location: index.php');
 }
 
 // logout
-if(isset($_POST['but_logout'])){
+if (isset($_POST['but_logout'])) {
     session_destroy();
     header('Location: login.php');
 }
@@ -48,26 +48,86 @@ function showNotification() {
 
 
 ?>
-<!doctype html>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Home</title>
+    <style>
+        #nav {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #333;
+        }
+
+        #nav li {
+            float: left;
+        }
+
+        #nav li a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        #nav li a:hover {
+            background-color: #111;
+        }
+
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <ul id="nav">
+        <li><a class="active" href="Home.html">Home</a></li>
+        <li><a href="About.html">About</a></li>
+        <li><a href="Search.html">Search</a></li>
+        <li><a href="Map.html">Map</a></li>
+        <li><a href="Chart.html">Chart</a></li>
+        <li><a href="Contact.html">Contact</a></li>
+        <li><a href="Login.html">Login</a></li>
+    </ul>
+
+    <h2>Home</h2>
+</body>
+
+</html>
+
+<!-- <!doctype html>
 <html>
-    <head></head>
-    <body onload="initNotifications()">
-	<ul id="nav">
-        	<li><a class="active" href="Home.html">Home</a></li>
-        	<li><a href="About.html">About</a></li>
-        	<li><a href="Search.html">Search</a></li>
-        	<li><a href="Map.html">Map</a></li>
-        	<li><a href="Chart.html">Chart</a></li>
-        	<li><a href="Contact.html">Contact</a></li>
-        	<li><a href="Login.html">Login</a></li>
+
+<head></head>
+
+<body onload="initNotifications()">
+    <ul id="nav">
+        <li><a class="active" href="Home.html">Home</a></li>
+        <li><a href="About.html">About</a></li>
+        <li><a href="Search.html">Search</a></li>
+        <li><a href="Map.html">Map</a></li>
+        <li><a href="Chart.html">Chart</a></li>
+        <li><a href="Contact.html">Contact</a></li>
+        <li><a href="Login.html">Login</a></li>
     </ul>
 
     <h2>Home</h2>
 
-        <h1>Homepage</h1>
-        <form method='post' action="">
-            <input type="submit" value="Logout" name="but_logout">
-        </form>
-        <button onclick="showNotification()">Show Notification</button>
-    </body>
-</html>
+    <h1>Homepage</h1>
+    <form method='post' action="">
+        <input type="submit" value="Logout" name="but_logout">
+    </form>
+    <button onclick="showNotification()">Show Notification</button>
+</body>
+
+</html> -->
