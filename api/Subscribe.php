b8eac8695ca13c3b43010ac5bd96880bfc525207
Jenny <lion_mango29@outlook.com>
2022-04-24 13:06:10 -0400

    Update

--- api/Subscribe.php
+++ api/Subscribe.php
@@ -11,30 +11,11 @@
     <?php
     include 'database.php';
 
-    /* if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $email = $_POST['mail'];
-        if (empty($email)) {
-            echo "Name is empty";
-        } else {
-            echo $email;
-        }
-    } */
-
     // Check Connection
     if ($con === false) {
         die("ERROR: Could not connect. "
             . mysqli_connect_error());
     }
-
-    // Taking all 5 values from the form data(input)
-    //$usr_name = $_REQUEST['u_name'];
-    //$pword = $_REQUEST['pword'];
-    //$email = $_REQUEST['Email'];
-
-    // Performing insert query execution
-    // here our table name is college
-    // before I inserted into "checkout"
-    //$sql = "INSERT INTO checkout(uname, pword) VALUES ('$usr_name', '$pword')";
 
     $email = $_POST['mail'];
     $sql = "INSERT INTO SUBSCRIBER(Email) VALUES ('$email')";
@@ -43,15 +24,12 @@
         echo "<h3>Data stored in a database successfully."
             . " Please browse your localhost php my admin"
             . " to view the updated data</h3>";
-
-        //echo nl2br("\n$first_name\n $last_name\n "
-        //	. "$gender\n $address\n $email");
     } else {
         echo "ERROR: Hush! Sorry $sql. "
             . mysqli_error($con);
     }
 
-    // Close connection
+    // Close Connection
     mysqli_close($con);
     ?>
 </body>
