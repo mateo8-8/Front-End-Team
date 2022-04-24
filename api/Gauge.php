8eae2f5cd593888e453f69979f0d694577783bfb
Jenny <lion_mango29@outlook.com>
2022-04-24 15:02:11 -0400

    Update

---
+++ api/Gauge.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Gauge Chart</title>
+    <style>
+        #nav {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #333;
+        }
+        
+        #nav li {
+            float: left;
+        }
+        
+        #nav li a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+        
+        #nav li a:hover {
+            background-color: #111;
+        }
+        
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        
+        #chart_div {
+            margin: auto;
+        }
+    </style>
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+        google.charts.load('current', {
+            'packages': ['gauge']
+        });
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+
+            var data = google.visualization.arrayToDataTable([
+                ['Label', 'Value'],
+                ['Temperature', 80],
+                ['Humidity', 55],
+                ['Pressure', 68],
+                ['Wind', 70],
+                ['Fire', 2]
+            ]);
+
+            var options = {
+                width: 800,
+                height: 240,
+                redFrom: 90,
+                redTo: 100,
+                yellowFrom: 75,
+                yellowTo: 90,
+                minorTicks: 5
+            };
+
+            var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
+
+            chart.draw(data, options);
+
+            setInterval(function() {
+                data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
+                chart.draw(data, options);
+            }, 13000);
+            setInterval(function() {
+                data.setValue(1, 1, 40 + Math.round(60 * Math.random()));
+                chart.draw(data, options);
+            }, 5000);
+            setInterval(function() {
+                data.setValue(2, 1, 60 + Math.round(20 * Math.random()));
+                chart.draw(data, options);
+            }, 26000);
+        }
+    </script>
+</head>
+
+<body>
+    <ul id="nav">
+        <li><a class="active" href="https://313wildfire.com/Front-End-Team/api/Home.php">Home</a></li>
+        <!-- <li><a href="https://313wildfire.com/Front-End-Team/pages/About.html">About</a></li> -->
+        <li><a href="https://313wildfire.com/Front-End-Team/api/OpenWeatherAPI.php">Open Weather API</a></li>
+        <li><a href="https://313wildfire.com/Front-End-Team/api/Search.php">Search</a></li>
+        <li><a href="https://313wildfire.com/Front-End-Team/pages/Map.html">Map</a></li>
+        <!-- <li><a href="https://313wildfire.com/Front-End-Team/pages/Chart.html">Chart</a></li> -->
+        <li><a href="https://313wildfire.com/Front-End-Team/pages/Combo.html">Combo Chart</a></li>
+        <li><a href="https://313wildfire.com/Front-End-Team/pages/Gauge.html">Gauge Chart</a></li>
+        <li><a href="https://313wildfire.com/Front-End-Team/pages/Contact.html">Contact</a></li>
+    </ul>
+
+    <h2>Chart</h2>
+
+    <button onclick="window.location.href='Combo.html'">Combo Chart</button>
+    <button onclick="window.location.href='Gauge.html'">Gauge Chart</button>
+
+    <div id="chart_div" style="width: 900px; height: 500px;"></div>
+</body>
+
+</html>
