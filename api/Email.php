4488b8390931f210fd23a831a866de0608bf9a82
Jenny <lion_mango29@outlook.com>
2022-04-24 12:20:01 -0400

    Update

--- api/Email.php
+++ api/Email.php
@@ -7,7 +7,7 @@
 
 <body>
     <form action="Subscribe.php" method="POST">
-        <input type="text" placeholder="Email address" name="mail">
+        <input type="text" placeholder="Email address" name="mail" size="50">
         <button id="subscribe">Subscribe</button>
     </form>
 </body>
