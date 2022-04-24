0f43910880e8d6c922d3753e49f3f0d453015f0b
esimmers <esimmers9@gmail.com>
2022-04-24 12:46:23 -0400

    add email functionality

---
+++ EmailPHP.php
@@ -0,0 +1,41 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require('/Email/Exception.php');
+require('/Email/PHPMailer.php');
+require('/Email/SMTP.php');
+
+// passing true in constructor enables exceptions in PHPMailer
+$mail = new PHPMailer(true);
+
+try {
+    // Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER; // for detailed debug output
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+
+    $mail->Username = 'sphost1a@gmail.com'; // YOUR gmail email
+    $mail->Password = 'Checkout!'; // YOUR gmail password
+
+    // Sender and recipient settings
+    $mail->setFrom('sphost1a@gmail.com', 'Admin');
+    $mail->addAddress('sphost1b@gmail.com', 'Personal User');
+    $mail->addReplyTo('sphost1a@gmail.com', 'Admin'); // to set the reply to
+
+    // Setting the email content
+    $mail->IsHTML(true);
+    $mail->Subject = "Warning Fire Detected";
+    $mail->Body = 'TESTING EMAIL';
+    $mail->AltBody = 'Test Email';
+
+    $mail->send();
+    echo "Email message sent.";
+} catch (Exception $e) {
+    echo "Error in sending email. Mailer Error: {$mail->ErrorInfo}";
+}
+?>
