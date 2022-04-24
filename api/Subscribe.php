f5cce974cc5fc2a9fa1a8abe50a997e563a7e1e9
Jenny <lion_mango29@outlook.com>
2022-04-24 12:59:27 -0400

    Update

--- api/Subscribe.php
+++ api/Subscribe.php
@@ -21,7 +21,7 @@
     }
 
     // Check Connection
-    if ($conn === false) {
+    if ($con === false) {
         die("ERROR: Could not connect. "
             . mysqli_connect_error());
     }
@@ -36,9 +36,9 @@
     // before I inserted into "checkout"
     //$sql = "INSERT INTO checkout(uname, pword) VALUES ('$usr_name', '$pword')";
 
-    $sql = "INSERT INTO SUBSCRIBER(SubscriberID, Email) VALUES ('1', '$email')";
+    $sql = "INSERT INTO SUBSCRIBER(Email) VALUES ('$email')";
 
-    if (mysqli_query($conn, $sql)) {
+    if (mysqli_query($con, $sql)) {
         echo "<h3>data stored in a database successfully."
             . " Please browse your localhost php my admin"
             . " to view the updated data</h3>";
@@ -47,11 +47,11 @@
         //	. "$gender\n $address\n $email");
     } else {
         echo "ERROR: Hush! Sorry $sql. "
-            . mysqli_error($conn);
+            . mysqli_error($con);
     }
 
     // Close connection
-    mysqli_close($conn);
+    mysqli_close($con);
     ?>
 </body>
 
