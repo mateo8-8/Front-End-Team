ea6d400f88b003e949b7113ec6aa79b7fdd1fd44
AtkinsChad <atkinschadt@gmail.com>
2022-04-26 12:55:35 -0400

    email restraint

--- api/Email.php
+++ api/Email.php
@@ -7,12 +7,12 @@
 
 <body>
     <form action="Subscribe.php" method="POST">
-        <input type="text" placeholder="Email address" name="mail" size="50">
+        <input type="email" placeholder="Email address" name="mail" size="50">
         <button id="subscribe">Subscribe</button>
     </form>
 
     <form action="Unsubscribe.php" method="POST">
-        <input type="text" placeholder="Email address" name="unsub" size="50">
+        <input type="email" placeholder="Email address" name="unsub" size="50">
         <button id="unsubscribe">Unsubscribe</button>
     </form>
 </body>
