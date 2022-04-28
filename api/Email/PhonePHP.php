10ba3a00fa805d313332de42c70806af32e86cba
esimmers <esimmers9@gmail.com>
2022-04-28 12:26:22 -0400

    PhonePHP

--- api/Email/PhonePHP.php
+++ api/Email/PhonePHP.php
@@ -29,15 +29,14 @@ try {
 
     // Sender and recipient settings
     $mail->setFrom('sphost1a@gmail.com', '313 Wild Fire Detection');
-    $mail->addAddress( "8044414385@txt.att.net", 'Personal User');
-    echo $row[0];
+
     
-    // while($row = mysqli_fetch_array($result)) {
-    //     // $var = "@txt.att.net";
-    //     $mail->addAddress( $row[0] .= "@txt.att.net", 'Personal User');
+    while($row = mysqli_fetch_array($result)) {
+        $var = "8044414385@txt.att.net";
+        $mail->addAddress( $var, 'Personal User');
 
       
-    // }
+    }
 
 
     $mail->addReplyTo('sphost1a@gmail.com', 'Admin'); // to set the reply to
