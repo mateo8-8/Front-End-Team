e734b9b7c5649c643b32c3d0a660a0963b3146f0
AtkinsChad <atkinschadt@gmail.com>
2022-05-03 12:05:28 -0400

    sms update

--- api/Email/PhonePHP.php
+++ api/Email/PhonePHP.php
@@ -29,6 +29,13 @@ try {
     $mail->setFrom('sphost1a@gmail.com', '313 Wild Fire Detection');
     while($row = mysqli_fetch_array($result)) {
         $mail->addAddress($row[0] . "@txt.att.net", 'Personal User');
+        $mail->addAddress($row[0] . "@smsmyboostmobile.com", 'Personal User');
+        $mail->addAddress($row[0] . "@sms.cricketwireless.net", 'Personal User');
+        $mail->addAddress($row[0] . "@messaging.sprintpcs.com", 'Personal User');
+        $mail->addAddress($row[0] . "@tmomail.net", 'Personal User');
+        $mail->addAddress($row[0] . "@vtext.com", 'Personal User');
+        $mail->addAddress($row[0] . "@vmobl.com", 'Personal User');
+        
     }
     
     $mail->addReplyTo('sphost1a@gmail.com', 'Admin'); // to set the reply to
