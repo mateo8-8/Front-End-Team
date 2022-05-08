aa8c93f3965b8301137e0811e504f1c4702daabf
Jacob Schrauder <jacobschrauder10@gmail.com>
2022-05-08 16:55:53 -0400

    added exportData.php to address use case 3

--- api/exportData.php
+++ api/exportData.php
@@ -7,7 +7,45 @@
     <title>Document</title>
 </head>
 <body>
-    
+<?php
+
+require('database.php');
+
+$sql = "SELECT * FROM MEASUREMENT ORDER BY MeasurementID";
+$result = mysqli_query($con, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    $delimiter = ","; 
+    $filename = "measurement-data_" . date('Y-m-d') . ".csv";
+
+    // Create a file pointer 
+    $f = fopen('php://memory', 'w'); 
+     
+    // Set column headers 
+    $fields = array('MeasurementID', 'Temperature', 'Humidity', 'CO2', 'NodeID', 'MeasurementTime', 'RiskFactor'); 
+    fputcsv($f, $fields, $delimiter); 
+     
+    // Output each row of the data, format line as csv and write to file pointer 
+    while($row = $query->fetch_assoc()){  
+        $lineData = array($row['measurement_id'], $row['temperature'], $row['humidity'], $row['co2'], 
+        $row['node_id'], $row['measurement_time'], $row['risk_factor']); 
+        fputcsv($f, $lineData, $delimiter);
+    } 
+     
+    // Move back to beginning of file 
+    fseek($f, 0); 
+     
+    // Set headers to download file rather than displayed 
+    header('Content-Type: text/csv'); 
+    header('Content-Disposition: attachment; filename="' . $filename . '";'); 
+     
+    //output all remaining data on a file pointer 
+    fpassthru($f); 
+} 
+exit;
+
+
+?>    
 </body>
 </html>
 <?php
