4eb418206ee8bca874aee1aba677d2069caad6ce
Jacob Schrauder <jacobschrauder10@gmail.com>
2022-05-08 17:10:26 -0400

    added downloadData.php

---
+++ api/downloadData.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Download Page</title>
+
+    
+</head>
+
+<body>
+    <form action="exportData.php" method="POST">
+
+        <button id="download">Download Measurement Data</button>
+    </form>
+
+    
+</body>
+
+</html>
